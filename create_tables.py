0566e04c1e47cb5b2d9d9f20375c399aeb1060ab
Samrudhi Ghanate <samrudhighanate27@gmail.com>
2026-07-18 13:31:12 +0530

    Add user authentication

--- create_tables.py
+++ create_tables.py
@@ -2,6 +2,7 @@ from app.core.database import Base, engine
 
 # Import all models here
 from app.models.project import Project
+from app.models.user import User
 
 Base.metadata.create_all(bind=engine)
 
